70adf7303e3bfb42c0be8ddab658f89122efbbb7
Simon Heath <icefoxen@gmail.com>
2019-01-31 15:25:07 -0500

    heyyyyyy it's not too hard so far.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # isildur
+
 Not *stealing* the Ring, honest, just borrowing it...
+
+Basically, the [`ring`](https://crates.io/crates/ring) crypto library for Rust is great,
+but its [yanking policy](https://github.com/briansmith/ring/issues/774) is a real pain in the ass.
+So, this is a program that will watch the crates.io index once a day, and if it sees a new
+version of `ring` is published, will fetch it (even if it's yanked) and re-publish it under
+a different crate name (`gnir`, 'cause, why not) which will never be yanked.
+
+If you want to write a library using `ring` without potentially breaking heckin' everything forever
+whenever one of your users tries to use `ring` as well, use `gnir` instead.  
+
+This tool republishes `ring` with no changes, other than the name.  Be aware that 
+using old versions of `ring` may expose you to security vulnerabilities, and that the original
+maintainer of it does not provide any support for older versions except through paid contracting.
+
+
+# Is this reliable?
+
+I need this functionality, so I'm intending to just have this thing running forever.  However, I
+won't be around forever, one way or another, so I'm providing this software to whoever wants
+it to implement their own set.
+
+If you don't trust that, feel free to deploy this software itself.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use crates_index;
+use std::collections::HashSet;
+
+fn mirror_crate(src_crate: &str, dest_crate: &str, version: &str) {
+    println!("Mirroring {} {} -> {} {}", src_crate, version, dest_crate, version);
+}
+
 fn main() {
-    println!("Hello, world!");
+    const SRC_CRATE: &str = "ring";
+    const DEST_CRATE: &str = "gnir";
+    const CRATE_INDEX_DIR: &str = "_index";
+
+    let index = crates_index::Index::new(CRATE_INDEX_DIR);
+    println!("Fetching crate index...");
+
+    index
+        .retrieve_or_update()
+        .expect("Could not fetch/update crate index.");
+    let src_crate = index.crates().find(|c| c.name() == SRC_CRATE)
+        .expect("The crate we're trying to mirror does not exist?");
+    let dest_crate = index.crates().find(|c| c.name() == DEST_CRATE);
+
+    let src_versions_to_mirror = if let Some(existing_dest) = dest_crate {
+        println!("Dest crate exists, filtering out known versions");
+        // O(n^2) is just fine if n is small, honest o/`
+        // Fiiiiine, it's simpler to do it right anyway.
+        let src_version_set: HashSet<&str> = src_crate.versions().iter().map(|v| v.version()).collect();
+        let versions_to_mirror: Vec<String> = existing_dest.versions().iter()
+            .map(|dest_version| dest_version.version()) // We just need the string.
+            .filter(|dest_version_str| src_version_set.contains(dest_version_str))
+        // If we collect to Vec<&str> then we can't return it 'cause
+        // all the &str's point into `existing_dest`, which is dropped
+        // at the end of this scope.  We COULD fiddle the order of things
+            // to make the ownership work, orrrrrrrrr...
+            .map(|v| v.to_owned())
+            .collect();
+        versions_to_mirror
+    } else {
+        println!("Dest crate does not exist, mirroring all src crate versions");
+        src_crate.versions().iter()
+            .map(|v| v.version()) // Just get the string
+            .map(|v| v.to_owned())
+            .collect()
+    };
+
+    src_versions_to_mirror.iter()
+        .for_each(|v| mirror_crate(&SRC_CRATE, &DEST_CRATE, v));
 }
